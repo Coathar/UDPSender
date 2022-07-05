1e6459ae2a06dd91dd7f08d87087b154043f6fd4
Coathar <trainman1234@gmail.com>
2022-07-04 21:29:17 -0500

    Add project files.

---
+++ UDPSender/NearbyPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPSender
+{
+    internal class NearbyPlayer
+    {
+        public string version { get; set; }
+
+        public string build { get; set; }
+
+        public string account { get; set; }
+
+        public string secondary_account { get; set; }
+
+        public long avatar { get; set; }
+
+        public int level { get; set; }
+
+        public long pframe { get; set; }
+
+        public int platform { get; set; }
+
+        public int elevel { get; set; }
+
+        public Endorsement[] endors { get; set; }
+
+        public class Endorsement
+        {
+            public long id { get; set; }
+
+            public int count { get; set; }
+        }
+
+        public static readonly long Shotcaller = 972777519512041796;
+        public static readonly long GoodTeammate = 972777519512041797;
+        public static readonly long Sportsmanship = 972777519512041798;
+    }
+}
---
+++ UDPSender/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using UDPSender;
+
+namespace UDPSender
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 5000;
+            timer.Elapsed += SendPacket;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+
+            Console.WriteLine("Press the Enter key to exit the program at any time... ");
+            Console.ReadLine();
+        }
+
+        private static void SendPacket(object source, System.Timers.ElapsedEventArgs e)
+        {
+            NearbyPlayer test = new NearbyPlayer()
+            {
+                version = "2",
+                build = "100672",
+                account = "<AccountID>",
+                secondary_account = "<AccountID>",
+                avatar = 166633186212728153,
+                level = 1953,
+                pframe = 166633186212712223,
+                platform = 1,
+                elevel = 2,
+                endors = new NearbyPlayer.Endorsement[]
+                {
+                    new NearbyPlayer.Endorsement()
+                    {
+                        id = NearbyPlayer.Shotcaller,
+                        count = 63
+                    },
+                    new NearbyPlayer.Endorsement()
+                    {
+                        id = NearbyPlayer.GoodTeammate,
+                        count = 1266
+                    },
+                    new NearbyPlayer.Endorsement()
+                    {
+                        id = NearbyPlayer.Sportsmanship,
+                        count = 622
+                    }
+                }
+            };
+
+            using (UdpClient client = new UdpClient(4242))
+            {
+                IPAddress address = IPAddress.Parse("224.0.0.5");
+
+                string jsonData = JsonConvert.SerializeObject(test);
+
+                Console.WriteLine("Sending nearby player packet");
+
+                byte[] encodedData = Encoding.ASCII.GetBytes(jsonData);
+
+                Console.WriteLine(jsonData);
+
+                IPEndPoint endpoint = new IPEndPoint(address, 4242);
+                client.Send(encodedData, encodedData.Length, endpoint);
+            }            
+        }
+    }
+}
+
+
